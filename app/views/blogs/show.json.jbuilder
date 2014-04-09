b24000b623c181049c3d56d251ec0755215ffecd
Sameer <saneerdgrea8@yahoo.com>
2014-04-09 21:17:13 +0500

    App with modal

---
+++ app/views/blogs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @blog, :id, :subject, :message, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exp::Application.config.secret_key_base = 'a23077f88a6e4aa147d1801c89a2fd8e86ee6954b64fc111204da66cded3d1838962a39d55f840f18a29d4462a9cdbd6a483b0715828bab55c5045a67811cc10'
